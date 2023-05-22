a1568c4441f162bab51706e71766b689a8eddd8c
Jeremy Gillard <gillard.jeremy@outlook.fr>
2023-05-22 21:27:59 +0200

    third commit: add Bloc component

---
+++ components/Bloc.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bloc">
+    <h2>This is a bloc.</h2>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam
+      praesentium, officia quis facilis dolorem quaerat voluptates, deleniti
+      minima magni laudantium atque cumque, animi officiis excepturi cum sint
+      maxime reprehenderit numquam!
+    </p>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
